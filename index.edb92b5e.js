a22a4ee5b5650440f8f1157ab3579e37617998cf
Stas <staskapica93@gmail.com>
2024-08-26 18:22:32 +0200

    make build

---
+++ index.edb92b5e.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),r=0;r<e.rows.length;r++){var l=e.rows[r].cells[1],o=e.rows[r].cells[3],c=l.cloneNode(!0);o.after(c)}
+//# sourceMappingURL=index.edb92b5e.js.map
